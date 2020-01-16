8ba9a0dac4cd9c68a0cbcbec501c5e6e0a2a96ad
Tin Unkai <tinunkai@gmail.com>
2020-01-16 14:26:14 +0900

    add dein

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
-
 set -x
 
+# set prezto for zsh
 rm -rf $HOME/.zprezto $HOME/.zlogin $HOME/.zlogout $HOME/.zpreztorc $HOME/.zprofile $HOME/.zshenv $HOME/.zshrc
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -13,3 +13,14 @@ done
 
 cp zsh/zshrc $HOME/.zshrc
 cp zsh/zpreztorc $HOME/.zpreztorc
+
+# dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm installer.sh
+
+mkdir -p $HOME/.config/nvim
+cp ./init.vim $HOME/.config/nvim/init.vim
+
+# pyenv
+curl https://pyenv.run | bash
