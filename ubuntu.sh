c1108acf56d7f53b4791fa6e0d69b491f078874e
Tin Unkai <tinunkai@gmail.com>
2020-02-22 12:35:34 +0900

    add ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,5 @@
+sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
