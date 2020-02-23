f1637d12564891f6a3d00cfb6c43a9a2a730eec0
Tin Unkai <tinunkai@gmail.com>
2020-02-23 03:39:02 +0000

    ubuntu -y

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
-sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-sudo apt-get install software-properties-common
+sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
-sudo apt install neovim
+sudo apt install neovim -y
