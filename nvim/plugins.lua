51ccfd3b72f7087d40eda1bceb680812c5458ca1
Tin Unkai <tinunkai@gmail.com>
2022-04-27 16:52:54 +0900

    add nvim-cmp

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -6,9 +6,20 @@ return require('packer').startup(function()
   use 'itchyny/lightline.vim'
   use 'racer-rust/vim-racer'
   use 'nvim-treesitter/nvim-treesitter'
+
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+
   use {
-    --'nvim-orgmode/orgmode',
+    -- 'nvim-orgmode/orgmode',
     'tinunkai/orgmode',
-    config = function() require('orgmode').setup {} end
+    config = function() require'orgmode'.setup {} end
   }
 end)
