ee547246a77bc4a8651ba9d3ba9061693bffe74c
Tin Unkai <tinunkai@gmail.com>
2022-05-18 10:50:50 +0900

    add slstatus.config.h

--- zsh.sh
+++ zsh.sh
@@ -11,7 +11,7 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 
 # zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+git clone https://github.com/zplug/zplug $HOME/.zplug
 
 cp zsh/zshrc $HOME/.zshrc
 cp zsh/zpreztorc $HOME/.zpreztorc
