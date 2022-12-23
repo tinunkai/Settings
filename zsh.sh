7e9f01916062c2ccbed4b624e837abde157e8d18
Tin Unkai <tinunkai@gmail.com>
2022-12-23 09:39:03 +0900

    delete zprezto

--- zsh.sh
+++ zsh.sh
@@ -3,16 +3,8 @@ set -x
 
 rm -rf $HOME/.zprezto $HOME/.zlogin $HOME/.zlogout $HOME/.zpreztorc $HOME/.zprofile $HOME/.zshenv $HOME/.zshrc $HOME/.zplug
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
 # zplug
 git clone https://github.com/zplug/zplug $HOME/.zplug
 
 cp zsh/zshrc $HOME/.zshrc
-cp zsh/zpreztorc $HOME/.zpreztorc
 cp zsh/main.omp.json $HOME/.config
