09b97481e96ead67c312672f2ef6bfe27c085107
Tin Unkai <tinunkai@gmail.com>
2023-01-11 12:10:41 +0900

    HackGen35 Console NFJ

--- suckless.config/st.config.h
+++ suckless.config/st.config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:size=12:antialias=true:autohint=true";
+static char *font = "HackGen35 Console NFJ:size=14:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
