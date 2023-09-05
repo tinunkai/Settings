310ea90eabd9a7883b9e953f38251ca309b070e0
Tin Unkai <tinunkai@gmail.com>
2023-09-05 13:43:40 +0900

    delete zshell plugins

--- zsh.sh
+++ zsh.sh
@@ -3,9 +3,6 @@ set -x
 
 rm -rf $HOME/.zprezto $HOME/.zlogin $HOME/.zlogout $HOME/.zpreztorc $HOME/.zprofile $HOME/.zshenv $HOME/.zshrc $HOME/.zplug
 
-# zplug
-git clone https://github.com/zplug/zplug $HOME/.zplug
-
 cp zsh/zshrc $HOME/.zshrc
 mkdir -p $HOME/.config
 cp zsh/main.omp.json $HOME/.config
