dc449589a5eae4246b4fd9247414c179562b33cd
Tin Unkai <tinunkai@gmail.com>
2023-09-07 11:08:54 +0900

    zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -4,4 +4,4 @@ set -x
 rm -rf $HOME/.zprezto $HOME/.zlogin $HOME/.zlogout $HOME/.zpreztorc $HOME/.zprofile $HOME/.zshenv $HOME/.zshrc $HOME/.zplug
 mkdir -p $HOME/.config
 
-cp zsh/zshrc $HOME/.zshrc
+cp zshrc $HOME/.zshrc
