a04bcfa2d4711384077874bc43f717f713cd45e8
Yunkai CHEN <yunkai.chen@fixstars.com>
2025-06-06 13:51:48 +0900

    [ubuntu.sh] install neovim from snap

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,4 @@
 set -x
 
 sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev zsh software-properties-common -y
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt update
-sudo apt install neovim -y
+sudo snap install nvim --classic
